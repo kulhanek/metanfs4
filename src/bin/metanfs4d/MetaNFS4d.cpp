a738a0067b0e38e6a47ecea2d71c66f06c8f38a3
Petr Kulhanek <kulhanek@chemi.muni.cz>
2017-05-07 19:01:38 +0200

    bugfix

--- src/bin/metanfs4d/MetaNFS4d.cpp
+++ src/bin/metanfs4d/MetaNFS4d.cpp
@@ -346,6 +346,8 @@ bool load_config(void)
     syslog(LOG_INFO,"cache file name (Name): %s",(const char*)CacheFileName);
 
     syslog(LOG_INFO,"-------------------------------------------------------------------------------");
+
+    return(true);
 }
 
 // -----------------------------------------------------------------------------
@@ -361,40 +363,43 @@ bool load_cache(bool skip)
     struct stat cstat;
     if( stat(CacheFileName,&cstat) != 0 ){
         syslog(LOG_INFO,"ignore cache - unable to stat the cache file %s",(const char*)CacheFileName);
-    } else {
-        if( (cstat.st_uid != 0) || (cstat.st_gid != 0) || ((cstat.st_mode & 0777) != 0644) ){
-            syslog(LOG_INFO,"wrong access rights on the cache file %s(%d:%d/%o) (root:root/0644 is required)",(const char*)CacheFileName,cstat.st_uid,cstat.st_gid,(cstat.st_mode & 0777));
-            return(false);
-        }
-
-        std::ifstream fin;
-        fin.open(CacheFileName);
-        int num = 0;
-        while( fin ){
-            char        type = '-';
-            std::string name;
-            int         nid = -1;
-            fin >> type >> name >> nid;
-            if( (fin) && (type == 'n') ){
-                UserToID[name] = nid;
-                IDToUser[nid] = name;
-                if( TopUserID < nid ){
-                    TopUserID = nid;
-                }
-                num++;
-            }
-            if( (fin) && (type == 'g') ){
-                GroupToID[name] = nid;
-                IDToGroup[nid] = name;
-                if( TopGroupID < nid ){
-                    TopGroupID = nid;
-                }
-                num++;
-            }
-        }
-        syslog(LOG_INFO,"cached items: %d",num);
-        fin.close();
+        return(true);
     }
+
+    if( (cstat.st_uid != 0) || (cstat.st_gid != 0) || ((cstat.st_mode & 0777) != 0644) ){
+        syslog(LOG_INFO,"wrong access rights on the cache file %s(%d:%d/%o) (root:root/0644 is required)",(const char*)CacheFileName,cstat.st_uid,cstat.st_gid,(cstat.st_mode & 0777));
+        return(false);
+    }
+
+    std::ifstream fin;
+    fin.open(CacheFileName);
+    int num = 0;
+    while( fin ){
+        char        type = '-';
+        std::string name;
+        int         nid = -1;
+        fin >> type >> name >> nid;
+        if( (fin) && (type == 'n') ){
+            UserToID[name] = nid;
+            IDToUser[nid] = name;
+            if( TopUserID < nid ){
+                TopUserID = nid;
+            }
+            num++;
+        }
+        if( (fin) && (type == 'g') ){
+            GroupToID[name] = nid;
+            IDToGroup[nid] = name;
+            if( TopGroupID < nid ){
+                TopGroupID = nid;
+            }
+            num++;
+        }
+    }
+    syslog(LOG_INFO,"cached items: %d",num);
+    fin.close();
+
+    return(true);
 }
 
 // -----------------------------------------------------------------------------
@@ -516,6 +521,8 @@ bool load_group(void)
     syslog(LOG_INFO,"group items already read from cache (users/groups): %d/%d",uinum,ginum);
     syslog(LOG_INFO,"users mapped to local users: %d",ulnum);
     fin.close();
+
+    return(true);
 }
 
 // -----------------------------------------------------------------------------
@@ -552,6 +559,8 @@ bool load_principal(void)
 
     syslog(LOG_INFO,"principalmap items (principal:local): %d",unum);
     fin.close();
+
+    return(true);
 }
 
 // -----------------------------------------------------------------------------
